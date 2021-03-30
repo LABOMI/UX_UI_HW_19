90baff29e05320ae85584e4ba3da7a69aab16ac7
LABOMI <hhanbom@gmail.com>
2021-03-29 18:24:54 -0700

    Adding hover and jquery
    Adding hover effect on nav. and download resume botton.
    Also tried to add the top scroll using jquery, but failed. Keep trying to figuring out.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-console.log("your index.js file is loaded correctly!");
+$(window).scroll(function() {
+    if ($(window).scrollTop(0) > 300) {
+      btn.addId('gototop');
+    } 
+    else {
+     btn.removeId('gototop');
+    }
+ });
+  
+$('#gototop').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
 
